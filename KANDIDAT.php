22ef7ce27e981529e48e45d8ef3da7c1492e3e87
pelangixenna <162612347+pelangixenna@users.noreply.github.com>
2024-07-19 23:12:39 +0700

    Add files via upload

---
+++ KANDIDAT.php
@@ -0,0 +1,18 @@
+<div class="col-lg-9 mt-2">
+        <div class="col">
+            <div class="card">
+                <img src="gambar/.jpg" class="card-img-top" alt=" Maaf, terjadi kesalahan">
+                <div class="card-body">
+                    <h5 class="card-title">Paslon urut 01</h5>
+                    <p class="card-text">cari gambar nanti.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img src="gambar/Soekarno.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Paslon urut 02</h5>
+                    <p class="card-text">jangan lupa cari gambar.</p>
+                </div>
+            </div>
